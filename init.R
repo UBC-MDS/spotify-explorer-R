0fd2af77c47680f9254263693f287123d8e1107d
Wenxin Xiang <xiangwx@student.ubc.ca>
2022-03-15 20:23:33 -0700

    update required packages

--- init.R
+++ init.R
@@ -9,5 +9,5 @@ options(repos=r)
 # ======================================================================
 
 # packages go here
-install.packages(c('dash', 'plotly', 'remotes', 'dashBootstrapComponents', 'dashHtmlComponents', 'dashCoreComponents', 'ggplot2', 'tidyverse', 'tidyr'))
+install.packages(c('dash', 'plotly', 'readr', 'here', 'ggthemes', 'purrr', 'dplyr', 'remotes', 'dashBootstrapComponents', 'dashHtmlComponents', 'dashCoreComponents', 'ggplot2'))
 remotes::install_github('facultyai/dash-bootstrap-components@r-release')
