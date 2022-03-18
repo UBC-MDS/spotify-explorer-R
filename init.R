e8c7654436c57903b719c923667d5afc9abbc573
michelle-wms <meisi.wg@gmail.com>
2022-03-17 22:17:14 -0700

    added pkgs

--- init.R
+++ init.R
@@ -9,5 +9,6 @@ options(repos=r)
 # ======================================================================
 
 # packages go here
-install.packages(c('dash', 'plotly', 'readr', 'here', 'purrr', 'dplyr', 'remotes', 'dashHtmlComponents', 'ggplot2', 'tidyverse'))
+install.packages(c('dash', 'plotly', 'readr', 'here', 'purrr', 'dplyr', 
+                'remotes', 'dashHtmlComponents', 'ggplot2', 'tidyverse', 'ggthemes', 'dashBootstrapComponents', 'dashCoreComponents'))
 remotes::install_github('facultyai/dash-bootstrap-components@r-release')
